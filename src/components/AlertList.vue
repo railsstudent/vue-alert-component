5fa6a95195ebd3f1c81e7d0c1aca074bcdbc0485
Connie Leung <cleung334@gmail.com>
2025-08-15 23:32:33 +0800

    feat: create an alert component

--- src/components/AlertList.vue
+++ src/components/AlertList.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
+import Alert from './Alert.vue'
 </script>
 
 <template>
-  <div role="alert" class="alert alert-info">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-    </svg>
-    <span>New software update available.</span>
-  </div>
+  <Alert />
   <div role="alert" class="alert alert-success">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
